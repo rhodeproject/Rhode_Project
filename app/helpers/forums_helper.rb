2d41542e87d1581146ccb1258b86e4e455287471
rhodeproject <mhatch73@gmail.com>
2012-05-09 22:03:00 -0400

    fixed last poster stuff

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -3,4 +3,8 @@ module ForumsHelper
     @forum = Forum.find(params[:id])
     @forum.users.where(:id => current_user.id).present?
   end
+
+  def latest_poster(user)
+    @user = User.find(user)
+  end
 end
