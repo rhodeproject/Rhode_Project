60581eff170212f7a052112dfdbb405386624b3a
rhodeproject <mhatch73@gmail.com>
2012-05-14 18:36:01 -0400

    changed email relay address

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => "587",
     :domain               => "gmail.com",
-    :user_name            => "mhatch73",
-    :password             => "cassidylynn",
+    :user_name            => "therhodeproject",
+    :password             => "Cas$idy1",
     :enable_starttls_auto => true
 
 }
