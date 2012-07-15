999cf1cf50c02a95e325a589bc65d497daa14696
Matt Hatch <mhatch73@gmail.com>
2012-07-15 19:42:06 -0400

    changed response to request

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -3,9 +3,9 @@ class SsoController < ApplicationController
 
   def create
 
-    @club = Club.find_by_token_id(response.headers[:token])
+    @club = Club.find_by_token_id(request.headers[:token])
     unless @club.nil?
-      @user = User.find_by_email(response.headers[:email])
+      @user = User.find_by_email(request.headers[:email])
       unless @user.nil?
         sign_in @user
         flash[:success] = "Welcome #{@user.name}"
