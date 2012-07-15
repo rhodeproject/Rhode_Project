6cc16d5492089ee405cf9a713e48123ef6879303
Matt Hatch <mhatch73@gmail.com>
2012-07-15 12:55:45 -0400

    corrected sso

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -1,6 +1,6 @@
 class SsoController < ApplicationController
   def create
-    club = Club.find_by_token(params[:token])
+    club = Club.find_by_token_id(params[:token])
     if club != nil
       user = User.find_by_email(params[:email])
       if user != nil
