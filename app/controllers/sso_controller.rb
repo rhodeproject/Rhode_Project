5d00d6c3a3f7a269d8c85c2af4e3cbfd057005ce
Matt Hatch <mhatch73@gmail.com>
2012-07-15 13:08:48 -0400

    sso and token

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -1,12 +1,13 @@
 class SsoController < ApplicationController
   def create
+
     club = Club.find_by_token_id(params[:token])
     if club != nil
       user = User.find_by_email(params[:email])
       if user != nil
         sign_in user
         flash[:success] = "Welcome #{user.name}"
-        redirect_back_or root_path
+        render root_path
       end
     end
   end
