f622b96d1f09b30ef33582df9407eb87ffd36982
Matt Hatch <mhatch73@gmail.com>
2012-07-15 16:14:26 -0400

    added csrf skip to API controller

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -2,14 +2,10 @@ class SsoController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def create
-    club = Club.find_by_token_id(params[:token])
-    #if club != nil
-      user = User.find_by_email(params[:email])
-      #if user != nil
-        sign_in user
-        flash[:success] = "Welcome #{user.name}"
-        render root_path
-    #  end
-   # end
+    @club = Club.find_by_token_id(params[:token])
+    @user = User.find_by_email(params[:email])
+    sign_in @user
+    flash[:success] = "Welcome #{@user.name}"
+    redirect_to root_path
   end
 end
