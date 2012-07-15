11b85fe83cba7463a8d98877c140ee18810d2301
Matt Hatch <mhatch73@gmail.com>
2012-07-15 19:35:26 -0400

    changed request.headers

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -2,9 +2,10 @@ class SsoController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def create
-    @club = Club.find_by_token_id(params[:token])
+
+    @club = Club.find_by_token_id(response.headers[:token])
     unless @club.nil?
-      @user = User.find_by_email(params[:email])
+      @user = User.find_by_email(response.headers[:email])
       unless @user.nil?
         sign_in @user
         flash[:success] = "Welcome #{@user.name}"
