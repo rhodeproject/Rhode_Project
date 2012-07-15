5aab6587b04bfbba9a412066ef3d8a3d4fbd037a
Matt Hatch <mhatch73@gmail.com>
2012-07-15 16:21:40 -0400

    fix sso

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -2,10 +2,15 @@ class SsoController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def create
+
     @club = Club.find_by_token_id(params[:token])
-    @user = User.find_by_email(params[:email])
-    sign_in @user
-    flash[:success] = "Welcome #{@user.name}"
-    redirect_to root_path
+    unless @club.nil?
+      @user = User.find_by_email(params[:email])
+      sign_in @user
+      flash[:success] = "Welcome #{@user.name}"
+      redirect_to root_path
+    else
+      refirect_to root_path
+    end
   end
 end
