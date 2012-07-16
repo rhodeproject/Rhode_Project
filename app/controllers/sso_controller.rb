d29c8c1ce765f9e5d24d663e660ce3d508567daf
Matt Hatch <mhatch73@gmail.com>
2012-07-15 21:56:19 -0400

    fixed controller

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -2,19 +2,16 @@ class SsoController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def create
-
-    #@club = Club.find_by_token_id(params[:token])
     @token = Token.find_by_api_token(params[:token])
     @user = User.find_by_email(params[:email])
     @club = Club.find_by_token_id(@token.id)
     if @user.club_id == @club.id
         sign_in @user
         flash[:success] = "Welcome #{@user.name}"
-        #redirect_to root_path
+        redirect_to root_path
+    else
+      flash[:warning] = "login failure using #{params[:token]}"
+      refirect_to root_path
     end
-
-      #flash[:warning] = "login failure using #{params[:token]}"
-      #refirect_to root_path
-
   end
 end
--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -4,10 +4,13 @@ def new
 end
 
 def create
-  @token = Token.new
-  @token.club = current_user.club
-  if @token.save
-    redirect_to current_user.club
+  club = Club.find(current_user.club_id)
+  @token = club.build_token
+  if club.save
+    club.update_attribute('token_id', @token.id)
+    club.save
+    #redirect_to current_user.club
+    flash[:success] = "SSO Token #{@token.api_token} has been added to #{club.name}"
   end
 end
 
