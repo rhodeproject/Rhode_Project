8980dfd554e17faf4b46450e0cd47a0032345d21
Matt Hatch <mhatch73@gmail.com>
2012-07-15 20:57:13 -0400

    ficed controller

--- app/controllers/sso_controller.rb
+++ app/controllers/sso_controller.rb
@@ -3,14 +3,15 @@ class SsoController < ApplicationController
 
   def create
 
-    @club = Club.find_by_token_id(params[:token])
-
-      @user = User.find_by_email(params[:email])
-      unless @user.nil?
+    #@club = Club.find_by_token_id(params[:token])
+    @token = Token.find_by_api_token(params[:token])
+    @user = User.find_by_email(params[:email])
+    @club = Club.find_by_token_id(@token.id)
+    if @user.club_id == @club.id
         sign_in @user
         flash[:success] = "Welcome #{@user.name}"
         #redirect_to root_path
-      end
+    end
 
       #flash[:warning] = "login failure using #{params[:token]}"
       #refirect_to root_path
