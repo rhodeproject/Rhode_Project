a9a1c3c6df6ac9f98761f124dc322fffc59585f0
Matt Hatch <mhatch73@gmail.com>
2012-07-16 21:05:05 -0400

    added replace sso token

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,17 +1,25 @@
 class TokensController < ApplicationController
-def new
-  @token = Token.new
-end
-
-def create
-  club = Club.find(current_user.club_id)
-  @token = club.build_token
-  if club.save
-    club.update_attribute('token_id', @token.id)
-    club.save
-    redirect_to current_user.club
-    flash[:success] = "SSO Token #{@token.api_token} has been added to #{club.name}"
+  def new
+    @token = Token.new
   end
-end
+
+  def create
+    club = Club.find(current_user.club_id)
+    remove_token(club)
+    @token = club.build_token
+    if club.save
+      club.update_attribute('token_id', @token.id)
+      club.save
+      redirect_to current_user.club
+      flash[:success] = "SSO Token added"
+    end
+  end
+
+private
+
+  def remove_token(club)
+    token = Token.find_by_club_id(club.id)
+    token.destroy unless token.nil?
+  end
 
 end
