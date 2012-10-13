e8287145a497d83c652927c06d124044b5658389
Matt Hatch <mhatch73@gmail.com>
2012-10-13 13:05:23 -0400

    hooks fix - check for user.nil?

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -6,23 +6,21 @@ class HooksController < ApplicationController
 
   def receiver
     #data_json = JSON.parse request.body.read
-
-    if params[:type] == "charge.succeeded"
-      make_active(params[:id])
+    @user = User.find_by_stripe_id(params[:id])
+    if params[:type] == "charge.succeeded" && !@user.nil?
+      make_active(@user)
     end
 
-    if params[:type] == "charge.failed"
-      make_inactive(params[:id])
+    if params[:type] == "charge.failed" && !@user.nil?
+      make_inactive(@user)
     end
   end
 
-  def make_active(id)
-    @user = User.find_by_stripe_id(id)
-    @user.activate_user
+  def make_active(user)
+    user.activate_user
   end
 
-  def make_inactive(id)
-    @user = User.find_by_stripe_id(id)
-    @user.inactivate_user
+  def make_inactive(user)
+    user.inactivate_user
   end
 end
