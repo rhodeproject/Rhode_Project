8329101538622183a74507eb6824652eae032b12
Matt Hatch <mhatch73@gmail.com>
2012-11-10 08:31:10 -0500

    config change

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RhodeProject::Application.config.secret_token = Figaro.env.application_secret_token#'0c6b091661de7c766ca43c1300d0e1bda046c49c400277eed40ee662f96eda882425aa9ffd233fdad4fd4a38dc8a6dda98d23e7d251d10ee01a31b88f4edcf05'
+RhodeProject::Application.config.secret_token = Figaro.env.application_secret_token
