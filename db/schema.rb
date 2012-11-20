d8f57ed6f2373386ee9299c69b6cd2b167a350bf
Matt Hatch <mhatch73@gmail.com>
2012-11-20 15:58:07 -0500

    fixed search issue

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -13,201 +12,6 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20121120195240) do
-
-  create_table "clubs", :force => true do |t|
-    t.string   "name"
-    t.string   "sub_domain"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
-    t.text     "welcome_message"
-    t.string   "heading1"
-    t.string   "heading2"
-    t.string   "heading3"
-    t.text     "message1"
-    t.text     "message2"
-    t.text     "message3"
-    t.integer  "token_id"
-    t.integer  "subscription_id"
-    t.boolean  "active"
-    t.text     "about"
-    t.string   "oath_token"
-    t.string   "oath_token_secret"
-    t.integer  "fee"
-    t.string   "stripe_publishable_key"
-    t.string   "stripe_api_key"
-    t.string   "contact_email"
-  end
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
-
-  create_table "events", :force => true do |t|
-    t.string   "name"
-    t.boolean  "all_day",     :default => false
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
-    t.integer  "club_id"
-    t.string   "title"
-    t.text     "description"
-    t.datetime "starts_at"
-    t.datetime "ends_at"
-    t.integer  "limit"
-  end
-
-  create_table "forums", :force => true do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.integer  "club_id"
-  end
-
-  create_table "forums_users", :id => false, :force => true do |t|
-    t.integer "forum_id"
-    t.integer "user_id"
-  end
-
-  add_index "forums_users", ["forum_id"], :name => "index_forums_users_on_forum_id"
-  add_index "forums_users", ["user_id"], :name => "index_forums_users_on_user_id"
-
-  create_table "lists", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "event_id"
-    t.string   "state"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "microposts", :force => true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "microposts", ["user_id", "created_at"], :name => "index_microposts_on_user_id_and_created_at"
-
-  create_table "notices", :force => true do |t|
-    t.string   "content"
-    t.integer  "club_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "posts", :force => true do |t|
-    t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "topic_id"
-    t.integer  "user_id"
-    t.integer  "club_id"
-  end
-
-  create_table "profiles", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "age"
-    t.text     "bio"
-    t.string   "blog"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "econtact_name"
-    t.string   "econtact_number"
-  end
-
-  create_table "relationships", :force => true do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  add_index "relationships", ["followed_id"], :name => "index_relationships_on_followed_id"
-  add_index "relationships", ["follower_id", "followed_id"], :name => "index_relationships_on_follower_id_and_followed_id", :unique => true
-  add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
-
-  create_table "sponsors", :force => true do |t|
-    t.integer  "club_id"
-    t.string   "name"
-    t.string   "image_name"
-    t.string   "url"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.text     "description"
-  end
-
-  create_table "subscriptions", :force => true do |t|
-    t.string   "email"
-    t.string   "stripe_customer_token"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
-  end
-
-  create_table "tokens", :force => true do |t|
-    t.string   "api_token"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "club_id"
-  end
-
-  create_table "topics", :force => true do |t|
-    t.string   "name"
-    t.integer  "last_poster_id"
-    t.datetime "last_post_at"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
-    t.integer  "forum_id"
-    t.integer  "user_id"
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.string   "password_digest"
-    t.string   "remember_token"
-    t.boolean  "admin",                  :default => false
-    t.boolean  "forum",                  :default => false
-    t.string   "reset_token"
-    t.datetime "password_reset_sent_at"
-    t.integer  "club_id"
-    t.boolean  "active",                 :default => false
-    t.string   "confirm_token"
-    t.datetime "anniversary"
-    t.string   "stripe_id"
-  end
-
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
-
-end
-=======
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
-
-ActiveRecord::Schema.define(:version => 20121112125029) do
 
   create_table "clubs", :force => true do |t|
     t.string   "name"
@@ -307,8 +111,9 @@ ActiveRecord::Schema.define(:version => 20121112125029) do
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "user_id"
     t.integer  "topic_id"
+    t.integer  "user_id"
+    t.integer  "club_id"
   end
 
   create_table "profiles", :force => true do |t|
@@ -388,4 +193,3 @@ ActiveRecord::Schema.define(:version => 20121112125029) do
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
->>>>>>> 08d769b52044b5531723d5ab25cebbd80268064b
