2bfffd5ad6195f90dbd7d3e4d2985891070bf054
Matt Hatch <mhatch73@gmail.com>
2013-09-18 10:00:16 -0400

    adjust the size of leader pic

--- app/assets/javascripts/leaders.js
+++ app/assets/javascripts/leaders.js
@@ -1,7 +1,1 @@
-$('document').ready(function(){
-   $('.gravatar-leader').hover(function(){
-       $(this).attr('src',$(this).attr('src').replace(/(s=)[^\&]+/, '$1' + '125'));
-   }, function(){
-       $(this).attr('src',$(this).attr('src').replace(/(s=)[^\&]+/, '$1' + '55'));
-   });
-});
+
